083ac5ab3bdc04a3e89bcac45094470f6539a56c
Vladyslav Ilchenko <Vladyslav_Ilchenko@epam.com>
2022-02-24 01:43:19 +0200

    All green

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -2,14 +2,24 @@
 {
     public class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
         public Product(string name, double price)
         {
             Name = name;
             Price = price;
         }
 
-        public string Name { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
 
-        public double Price { get; set; }
+            var product = (Product)obj;
+            return (Name == product.Name) && (Price == product.Price);
+        }
     }
 }
